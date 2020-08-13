df05ac22f589e246889d2cabad3ae5e00cfb6049
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-08-13 14:01:54 +0200

    Initial commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "dittnav-meta-repo"
+
+includeBuild("dittnav")
+includeBuild("dittnav-api")
+includeBuild("dittnav-legacy-api")
+includeBuild("dittnav-event-handler")
+includeBuild("dittnav-eventer-modia")
+includeBuild("dittnav-docker-compose")
+includeBuild("dittnav-varsel-bestiller")
+includeBuild("dittnav-event-aggregator")
+includeBuild("brukernotifikasjon-schemas")
+includeBuild("dittnav-event-test-producer")
+includeBuild("brukernotifikasjon-topic-iac")
+includeBuild("dittnav-periodic-metrics-reporter")
+includeBuild("dittnav-nightly-usage-statistics-reporter")
